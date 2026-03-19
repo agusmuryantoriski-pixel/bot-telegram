fd33b757709f08e2ff8d1c35bc12286d5bb5058f
agusmuryantoriski-pixel <agusmuryantoriski@gmail.com>
2026-03-19 09:23:52 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Bot aktif 🚀");
+});
